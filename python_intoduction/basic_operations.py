a72848f794d815731d2bd6dc661fa0486a3f656c
Nathaniel Quansah <nathquansah44@gmail.com>
2025-11-08 20:06:41 +0000

    Add basic arithmetic operations script

---
+++ python_intoduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print(f"{number1} + {number2} = {addition}")
+print(f"{number1} - {number2} = {subtraction}")
+print(f"{number1} * {number2} = {multiplication}")
