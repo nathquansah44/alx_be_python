8029a37f6475902e2b5dc604b319b115b91c2fc3
Nathaniel Quansah <nathquansah44@gmail.com>
2025-11-15 19:38:35 +0000

    Fix operation input prompt to match grading requirememts

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,7 @@
 # multiplication_table.py
 # Prompt the user for a number
-number = int(input("Enter a number to generate its multiplication table: "))
+number = int(input("Enter a number to generate its multiplication table:"))
 # use a for loop to generate the multiplication table fron 1 to 10
 for i in range(1, 11):
     result = number * i
     print(f"{number} x {i} = {result}")
-    
