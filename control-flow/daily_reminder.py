83f10d50a5c89caa039653aadc18a195dc82fa18
Nathaniel Quansah <nathquansah44@gmail.com>
2025-11-15 20:17:49 +0000

    fix operaation input prompt to match grading requirements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
 # daily_reminder.py 
 # Prompt the user for a single task
-task = input("Enter your task: ")
+Task = input("Enter your task:")
 # Prompt for the task's priority 
-priority = input("Enter the priority level (high/medium/low):").lower()
+Priority = input("Priority(high/medium/low):").lower()
 # Ask if the task is time-bound
-time_bound = input("Is it time-bound? (yes/no): ").lower()
+Time_Bound = input("Is it time-bound?(yes/no):").lower()
 # use match-case to handle different priorities
-match priority:
+match Priority:
     case "high":
-        reminder = f" '{task}' is a high priority task"
+        reminder = f" '{Task}' is a high priority task"
     case "medium":
-        reminder = f" '{task}' is a medium priority task"
+        reminder = f" '{Task}' is a medium priority task"
     case "low":
-        reminder = f" '{task}' is a low priority task"
+        reminder = f" '{Task}' is a low priority task"
     case _:
-        reminder = f" '{task}' has an unspecified priority level"
+        reminder = f" '{Task}' has an unspecified priority level"
 # use if statement to modify reminder if task is time-bound
-if time_bound == "yes":
+if Time_Bound == "yes":
     reminder += " that requires immediate attention today!"
 else:
     reminder += ". Consider completing it when you have free time."
