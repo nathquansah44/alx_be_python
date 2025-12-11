429f3fa00bf8d26ed962b314f07d3362aaa1aaca
Nathaniel Quansah <nathquansah44@gmail.com>
2025-12-11 21:03:54 +0000

    Format display_balance to show 2 decimal places

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,4 +20,4 @@ class BankAccount:
 
     def display_balance(self):
         """Print the current account balance in a user-friendly format."""
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
